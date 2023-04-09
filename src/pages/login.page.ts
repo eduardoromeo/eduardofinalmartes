815de96aa139bf8e7700f978f877cb3388bbde5f
eduardoromeo <eduram_mt@yahoo.es>
2023-04-09 18:41:21 -0400

    add result

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -2,15 +2,19 @@ import { BasePage } from "./base.page";
 import { ElementActions } from "../core/element-actions";
 
 export class LoginPage extends BasePage {
-    private username: string = '//input[@id="input-email"]';
-    private password: string = '//input[@id="input-password"]';
-    private buttonSign: string = '//input[@value="Login"]';
+    private username: string = '//input[@id="user"]';
+    private buttonContinue: string = '//input[@id="login"]';
+    private password: string = '//input[@id="password"]';
+    private buttonSign: string = '//div[@class="LgbsSe-bN97Pc"]';
     private selectorHome: string = '//a[text()="Your Store"]';
     constructor() {
         super()
     }
     async setUserName(user: string) {
         await ElementActions.setText(this.username, user)
+    }
+    async clickButtoncontinue() {
+        await ElementActions.click(this.buttonContinue)
     }
     async setPassword(pass: string) {
         await ElementActions.setText(this.password, pass)
--- userData.ts
+++ userData.ts
@@ -1,6 +1,6 @@
 export const configPage = {
-    linkLogin: 'https://thetestingworld.com/shop/index.php?route=account/login',
-    userName: 'arvd20197@gmail.com',
-    passName: 'Testing2023',
+    linkLogin: 'https://friday.app/p/asana-overview',
+    userName: 'eduramromeo77@gmail.com',
+    passName: 'ROMEO76992686ROMEO',
     lapseTime: (60 * 1000)
 }
